37250f9c791cf24898916555f1f601c8cbe0f13d
armyhcz <32027685+armyhcz@users.noreply.github.com>
2023-03-22 16:12:42 +0800

    sync

--- internal/start/start.go
+++ internal/start/start.go
@@ -8,12 +8,21 @@ type Result struct {
 
 func Start(ctx context.Context, concurrency int) chan *Result {
 	ch := make(chan *Result, 1024)
+	wg := &sync.WaitGroup{}
 	for i := 0; i < concurrency; i++ {
+		wg.Add(1)
 		go doStart(ctx, ch)
 	}
+	
+	go func() {
+		wg.Wait()
+		close(ch)
+	}()
+	
 	return ch
 }
 
 func doStart(ctx context.Context, result chan<- *Result) {
+	defer wg.Done()
 	// doStart
 }
